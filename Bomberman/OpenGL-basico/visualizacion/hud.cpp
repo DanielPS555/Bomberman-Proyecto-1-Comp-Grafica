27dddc1ed45f11c7c51c7f1d10c05ce759799974
matias.mastrangelo <matias.mastrangelo@fing.com.uy>
2024-05-05 20:16:43 -0300

    func in my pc

--- Bomberman/OpenGL-basico/visualizacion/hud.cpp
+++ Bomberman/OpenGL-basico/visualizacion/hud.cpp
@@ -304,7 +304,7 @@ void Hud::renderText(float resizeFont, std::string texto, float bordeSupIsqX, fl
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, texture_format, w_text, h_text, 0, texture_format, GL_UNSIGNED_BYTE, textSurface->pixels);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w_text, h_text, 0, GL_RGBA, GL_UNSIGNED_BYTE, textSurface->pixels);
 
 
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
